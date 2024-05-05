4a9546916e2fcd7d96865e000c6f927aea571d5d
Duong Nguyen <duongduongpho@gmail.com>
2024-05-05 16:28:42 +0700

    Cap nhat anh profile

--- Apps/Screens/Home/HomeScreen.jsx
+++ Apps/Screens/Home/HomeScreen.jsx
@@ -1,7 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { supabase } from '../../Utils/SupabaseConfig'
 
 export default function HomeScreen() {
+    const { user } = useUser();
+
+    useEffect(() => {
+        user && updateProfileImage();
+    }, [user])
+
+    const updateProfileImage = async () => {
+        const { data, error } = await supabase
+            .from('Users')
+            .update({ 'profileImage': user?.imageUrl })
+            .eq('email', user?.primaryEmailAddress?.emailAddress)
+            .is('profileImage', null)
+            .select();
+        console.log(data);
+    }
     return (
         <View style={{ padding: 50 }}>
             <Text>HomeScreen</Text>
