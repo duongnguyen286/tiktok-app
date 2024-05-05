72e5ef15a7ad449306639a21cebb089ee988fd22
Duong Nguyen <duongduongpho@gmail.com>
2024-05-05 11:24:06 +0700

    Add font

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontFamily: 'outfit-bold'
+      }}>Hello Duong Nguyen!</Text>
       <StatusBar style="auto" />
     </View>
   );
